83ea9039280c36edf7e6db404b60c1045c8a7eba
Redox <mail@redox.to>
2024-06-03 05:02:24 +0200

    Add boreas night cleanup

---
+++ lua/map_scripts/maps/gm_boreas_night.lua
@@ -0,0 +1,26 @@
+local removeThese = {
+    env_steam = true,
+    env_sprite = true,
+    prop_ragdoll = true,
+    light_dynamic = true,
+    env_soundscape = true,
+    func_dustmotes = true,
+    ambient_generic = true,
+    point_spotlight = true,
+    func_illusionary = true,
+    env_physexplosion = true,
+    env_fog_controller = true,
+    func_reflective_glass = true
+}
+
+local function isMapEnt( ent )
+    return ent:MapCreationID() ~= -1
+end
+
+hook.Add( "InitPostEntity", "GMS_BoreasCleaner", function()
+    for _, ent in ipairs( ents.GetAll() ) do
+        if removeThese[ent:GetClass()] and isMapEnt( ent ) then
+            SafeRemoveEntity( ent )
+        end
+    end
+end )
